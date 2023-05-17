b89898fa6d3163c69eeb1e9e6c31a06843056f51
António Fialho <antonio.ramalho.fialho@gmail.com>
2023-05-17 22:07:17 +0100

    Commit

--- app/src/main/java/pt/ipt/dama/fragments/MainActivity.kt
+++ app/src/main/java/pt/ipt/dama/fragments/MainActivity.kt
@@ -4,8 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    // create the object of type 'fragment' to use
+    lateinit var f1: MyFragment
+    lateinit var f2: MyFragment
+    lateinit var f3: MyFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // initialize the fragments
+        f1 = MyFragment.newInstance("fragment", "one")
+        f2 = MyFragment.newInstance("fragment", "two")
+        f3 = MyFragment.newInstance("fragment", "three")
+
+        val fragmentTransaction=supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragment1, f1)
+        fragmentTransaction.add(R.id.fragment2, f2)
+        fragmentTransaction.add(R.id.fragment3, f3)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+
     }
 }
---
+++ app/src/main/java/pt/ipt/dama/fragments/MyFragment.kt
@@ -0,0 +1,79 @@
+package pt.ipt.dama.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MyFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MyFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    /**
+     * lets write the code that the fragment will use
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        //at fragment it is not possible to access activity' elements
+        //you must use a View object
+        val view: View = inflater.inflate(R.layout.fragment_my, container, false)
+        val txt: TextView=view.findViewById(R.id.textView)
+        txt.text= "$param1 $param2" // <=> param1 +  " " + param2
+
+        //access to the button of fragment
+        val btn:Button=view.findViewById(R.id.button)
+        btn.setOnClickListener{
+            Toast.makeText(this.context, txt.text,Toast.LENGTH_SHORT).show()
+        }
+
+        //returns the fragment action
+        return view
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MyFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MyFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
